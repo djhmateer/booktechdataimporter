de8a9967d760746a25e1c075c0d99425d53642b3
Dave Mateer <davemateer@gmail.com>
2016-03-01 05:54:34 +0000

    refactoring Three shorter into smaller methods.. have class level variables, and trouble with not testing

--- ImporterConsole/3Shorter/AuthorAndBookGenerator.cs
+++ ImporterConsole/3Shorter/AuthorAndBookGenerator.cs
@@ -4,15 +4,24 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 
-
 namespace Three
 {
     // Make fake Author and Book data from firstname, surname, and word
     // text files for testing BookTech website
-    class Program
+    class AuthorAndBookGenerator
     {
+        static Random rnd;
+        static List<string> firstnames;
+        static List<string> surnames;
+        static List<string> words;
+
         static void Main()
         {
+            rnd = new Random();
+            firstnames = File.ReadAllLines(@"firstnames.txt").ToList();
+            surnames = File.ReadAllLines(@"surnames.txt").ToList();
+            words = File.ReadAllLines(@"words.txt").ToList();
+
             for (int i = 0; i < 5; i++)
             {
                 var authorID = InsertRandomlyNamedAuthorAndReturnID();
@@ -20,37 +29,12 @@ namespace Three
             }
         }
 
-        private static void AssignRandomNumberOfBookTitlesToAuthor(int authorID)
-        {
-            var words = File.ReadAllLines(@"words.txt").ToList();
-            using (var connection = Util.GetOpenConnection())
-            {
-                var rnd = new Random();
-                for (int j = 0; j < rnd.Next(1, 7); j++)
-                {
-                    using (var cmd = new SqlCommand("INSERT INTO Books(Title, AuthorID) VALUES(@Title, @AuthorID)", connection))
-                    {
-                        var bookTitle = GenerateBookTitle(words, rnd);
-
-                        cmd.Parameters.AddWithValue("@Title", bookTitle);
-                        cmd.Parameters.AddWithValue("@AuthorID", authorID);
-
-                        cmd.ExecuteScalar();
-                    }
-                }
-            }
-        }
-
         private static int InsertRandomlyNamedAuthorAndReturnID()
         {
-            var firstnames = File.ReadAllLines(@"firstnames.txt").ToList();
-            var surnames = File.ReadAllLines(@"surnames.txt").ToList();
-
             using (var connection = Util.GetOpenConnection())
             {
                 using (var cmd = new SqlCommand("INSERT INTO [Authors](FirstName, LastName,EmailAddress) VALUES(@FirstName, @LastName, @EmailAddress); SELECT SCOPE_IDENTITY()", connection))
                 {
-                    var rnd = new Random();
                     string authorFirstname = firstnames[rnd.Next(firstnames.Count)];
                     string authorSurname = surnames[rnd.Next(surnames.Count)].CapitaliseFirstLetter();
                     string autorEmail = authorFirstname + "@" + authorSurname + ".com";
@@ -62,7 +46,24 @@ namespace Three
             }
         }
 
-        private static string GenerateBookTitle(List<string> words, Random rnd)
+        private static void AssignRandomNumberOfBookTitlesToAuthor(int authorID)
+        {
+            using (var connection = Util.GetOpenConnection())
+            {
+                for (int j = 0; j < rnd.Next(1, 7); j++)
+                {
+                    using (var cmd = new SqlCommand("INSERT INTO Books(Title, AuthorID) VALUES(@Title, @AuthorID)", connection))
+                    {
+                        var bookTitle = GenerateBookTitle();
+                        cmd.Parameters.AddWithValue("@Title", bookTitle);
+                        cmd.Parameters.AddWithValue("@AuthorID", authorID);
+                        cmd.ExecuteScalar();
+                    }
+                }
+            }
+        }
+
+        private static string GenerateBookTitle()
         {
             string firstWord = words[rnd.Next(words.Count)].CapitaliseFirstLetter();
             string secondWord = words[rnd.Next(words.Count)].CapitaliseFirstLetter();
